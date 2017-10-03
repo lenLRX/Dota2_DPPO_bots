21f4279b0bd09d11a340283d7eaf8742e4e96076
lenLRX <545976176@qq.com>
2017-10-03 10:19:55 +0800

    smaller var

---
+++ cppSimulator/include/simulator.h
@@ -0,0 +1,10 @@
+#ifndef __SIMULATOR_H__
+#define __SIMULATOR_H__
+
+#include <Python.h>
+
+typedef struct {
+    PyObject_HEAD
+} cppSimulatorObject;
+
+#endif//__SIMULATOR_H__
--- cppSimulator/src/simulator.cpp
+++ cppSimulator/src/simulator.cpp
@@ -1,8 +1,21 @@
-#include <Python.h>
+#include "simulator.h"
 
-typedef struct {
-    PyObject_HEAD
-} cppSimulatorObject;
+static int
+cppSimulator_init(cppSimulatorObject* self, PyObject *args, PyObject *kwds) {
+    return 0;
+}
+
+static PyObject*
+cppSimulator_get_time(cppSimulatorObject* self) {
+    return PyLong_FromLong(0L);
+}
+
+static PyMethodDef cppSimulator_methods[] = {
+    { "get_time", (PyCFunction)cppSimulator_get_time, METH_NOARGS,
+    "get time of simulator"
+    },
+    { NULL }  /* Sentinel */
+};
 
 static PyTypeObject cppSimulatorType = {
     PyVarObject_HEAD_INIT(NULL, 0)
@@ -24,8 +37,26 @@ static PyTypeObject cppSimulatorType = {
     0,                         /* tp_getattro */
     0,                         /* tp_setattro */
     0,                         /* tp_as_buffer */
-    Py_TPFLAGS_DEFAULT,        /* tp_flags */
-    "cppSimulatorObject",           /* tp_doc */
+    Py_TPFLAGS_DEFAULT |
+    Py_TPFLAGS_BASETYPE,       /* tp_flags */
+    "cppSimulatorObject",      /* tp_doc */
+    0,                         /* tp_traverse */
+    0,                         /* tp_clear */
+    0,                         /* tp_richcompare */
+    0,                         /* tp_weaklistoffset */
+    0,                         /* tp_iter */
+    0,                         /* tp_iternext */
+    cppSimulator_methods,      /* tp_methods */
+    0,                         /* tp_members */
+    0,                         /* tp_getset */
+    0,                         /* tp_base */
+    0,                         /* tp_dict */
+    0,                         /* tp_descr_get */
+    0,                         /* tp_descr_set */
+    0,                         /* tp_dictoffset */
+    (initproc)cppSimulator_init,      /* tp_init */
+    0,                         /* tp_alloc */
+    0,                         /* tp_new */
 };
 
 static PyModuleDef cppSimulatorModule = {
