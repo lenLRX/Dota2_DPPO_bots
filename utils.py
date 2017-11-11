40cc08f5d71633b4033d145bf992017f155aa6b8
lenLRX <545976176@qq.com>
2017-11-11 11:41:26 +0800

    -0.01

--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ class Params():
         self.game_duriation = 200
         self.tick_per_action = 2
         self.game_per_update = 20
-        self.lr = 1e-3
+        self.lr = 1e-4
         self.gamma = 0.99
         self.gae_param = 0.95
         self.clip = 0.2
