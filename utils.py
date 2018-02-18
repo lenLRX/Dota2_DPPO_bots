392370ad28ca9f684433a5a9f996d50bb6ccee6b
lenLRX <545976176@qq.com>
2018-02-18 17:35:42 +0800

    more dim

--- utils.py
+++ utils.py
@@ -101,7 +101,7 @@ class Params():
         self.update_treshold = 2 - 1
         self.max_episode_length = 100
         self.seed = int(time.time())
-        self.num_inputs = {"env_input":6,"atk_target":2}
+        self.num_inputs = {"env_input":10,"atk_target":2}
         self.num_outputs = 3
         self.log_std_bound = 1
         self.use_lstm = False
